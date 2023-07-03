93fdced567bb02cdc00480dc19b562399eeb5f61
tilongzs <tilongzs@qq.com>
2023-07-03 17:19:15 +0800

    完成行走的基本控制。

---
+++ Source/UE_NetworkDemo/Character/BlacterCharacter.cpp
@@ -0,0 +1,118 @@
+﻿#include "BlacterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
+#include "../Common/Utils.h"
+
+ABlacterCharacter::ABlacterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	_cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	_cameraBoom->SetupAttachment(GetMesh());
+	_cameraBoom->TargetArmLength = 600.f;
+	_cameraBoom->bUsePawnControlRotation = true; // 使用Pawn身上的PlayerController控制弹簧臂
+	_followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	_followCamera->SetupAttachment(_cameraBoom, USpringArmComponent::SocketName);
+	_followCamera->bUsePawnControlRotation = false;
+}
+
+void ABlacterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABlacterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABlacterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// 参考教程 https://dev.epicgames.com/community/learning/tutorials/aqrD/unreal-engine-enhanced-input-binding-with-gameplay-tags-c
+
+	if (APlayerController* pc = CastChecked<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 100);
+		}
+	}
+
+	if (UEnhancedInputComponent* inputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_MoveForward)
+		{
+			inputComponent->BindAction(IA_MoveForward, ETriggerEvent::Triggered, this, &ABlacterCharacter::ActionMoveForward);
+		}
+
+		if (IA_MoveRight)
+		{
+			inputComponent->BindAction(IA_MoveRight, ETriggerEvent::Triggered, this, &ABlacterCharacter::ActionMoveRight);
+		}
+
+		if (IA_Turn)
+		{
+			inputComponent->BindAction(IA_Turn, ETriggerEvent::Triggered, this, &ABlacterCharacter::ActionTurn);
+		}
+
+		if (IA_LookUp)
+		{
+			inputComponent->BindAction(IA_LookUp, ETriggerEvent::Triggered, this, &ABlacterCharacter::ActionLookUp);
+		}
+
+		if (IA_Jump)
+		{
+			inputComponent->BindAction(IA_Jump, ETriggerEvent::Started, this, &ABlacterCharacter::ActionJump);
+		}
+	}
+}
+
+void ABlacterCharacter::ActionMoveForward(const FInputActionValue& inputActionValue)
+{
+	float inputValue = inputActionValue.Get<float>();
+	//Log(FString::Printf(TEXT("ActionMoveForward value:%f"), inputValue));
+	if (Controller && inputValue != 0)
+	{
+		const FRotator controllerYawRotator(0, Controller->GetControlRotation().Yaw, 0);		
+		const FVector direction = controllerYawRotator.RotateVector(FVector::ForwardVector); //const FVector direction(FRotationMatrix(controllerYawRotator).GetUnitAxis(EAxis::X));
+		AddMovementInput(direction, inputValue);
+	}
+}
+
+void ABlacterCharacter:: ActionMoveRight(const FInputActionValue& inputActionValue)
+{
+	float inputValue = inputActionValue.Get<float>();
+	//Log(FString::Printf(TEXT("ActionMoveForward value:%f"), inputValue));
+	if (Controller && inputValue != 0)
+	{
+		const FRotator controllerYawRotator(0, Controller->GetControlRotation().Yaw, 0);
+		const FVector direction = controllerYawRotator.RotateVector(FVector::RightVector);
+		AddMovementInput(direction, inputValue);
+	}
+}
+
+void ABlacterCharacter::ActionJump(const FInputActionValue& inputActionValue)
+{
+	Jump();
+}
+
+void ABlacterCharacter::ActionLookUp(const FInputActionValue& inputActionValue)
+{
+	float inputValue = inputActionValue.Get<float>();
+	AddControllerPitchInput(inputValue);
+}
+
+void ABlacterCharacter::ActionTurn(const FInputActionValue& inputActionValue)
+{
+	float inputValue = inputActionValue.Get<float>();
+	AddControllerYawInput(inputValue);
+}
+
+
+
+
---
+++ Source/UE_NetworkDemo/Character/BlacterCharacter.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/inputActionValue.h"
+#include "BlacterCharacter.generated.h"
+
+UCLASS()
+class UE_NETWORKDEMO_API ABlacterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlacterCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+		class USpringArmComponent* _cameraBoom;
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+		class UCameraComponent* _followCamera;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="EnhancedInput")
+		class UInputMappingContext* InputMappingContext;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EnhancedInput|Action")
+		class UInputAction* IA_MoveForward;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EnhancedInput|Action")
+		class UInputAction* IA_MoveRight;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EnhancedInput|Action")
+		class UInputAction* IA_Turn;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EnhancedInput|Action")
+		class UInputAction* IA_LookUp;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "EnhancedInput|Action")
+		class UInputAction* IA_Jump;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void ActionMoveForward(const FInputActionValue& inputActionValue);
+	UFUNCTION()
+	void ActionMoveRight(const FInputActionValue& inputActionValue);
+	UFUNCTION()
+	void ActionJump(const FInputActionValue& inputActionValue);
+	UFUNCTION()
+	void ActionLookUp(const FInputActionValue& inputActionValue);
+	UFUNCTION()
+	void ActionTurn(const FInputActionValue& inputActionValue);
+};
